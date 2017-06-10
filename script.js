2199de65cc548de47c58a42c76665a25eabb4745
Igor Oczkowicz <lolwatt221@gmail.com>
2017-06-10 16:25:40 +0200

    Create script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ var igor = "Gapisz się w"
 var czas = 1
 var placek = "ekran już"
 setInterval(function () {
-$('#test').append('<p>' + igor + ' ' + placek + ' ' + czas + 's' + '</p>');
+$('#test').append('<p>' + igor + ' ' + placek + ' ' + czas++ + 's' + '</p>');
 }, 1000);
 
